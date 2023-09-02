f8bcafc7f3f122889f24fc5348cf77193fdd6fc1
freshremix <90133873+freshremix@users.noreply.github.com>
2023-09-02 12:26:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import subprocess
 
-# Bash script to run
-bash_script = "bash <(wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh 2> /dev/null)"
+# Bash command to run (split into individual arguments)
+bash_command = ["bash", "-c", 'wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh | bash']
 
-# Run the Bash script as a subprocess
+# Run the Bash command as a subprocess
 try:
-    subprocess.run(bash_script, shell=True, check=True)
+    subprocess.run(bash_command, check=True)
 except subprocess.CalledProcessError as e:
     print(f"Error: {e}")
